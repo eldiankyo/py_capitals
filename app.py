14618d0d058612484f57873c304e5b2739adebd4
eldiankyo <eldian.kyo@gmail.com>
2022-09-18 02:35:17 +0000

    finished app

---
+++ app.py
@@ -0,0 +1,31 @@
+from tkinter import Tk, simpledialog, messagebox
+
+print('Countries and Capitals')
+root = Tk()
+root.withdraw()
+world = {}
+
+def fetchData():
+    with open('data.txt') as data:
+        for line in data:
+            line = line.rstrip('\n')
+            country, capital = line.split('/')
+            world[country] = capital
+
+def addData(country, capital):
+    with open('data.txt', 'a') as data:
+        data.write('\n' + country + '/' + capital)
+
+fetchData()
+
+while True:
+    wantedCountry = simpledialog.askstring('Country', 'Give me the name of the country:')
+    if wantedCountry in world:
+        capital = world[wantedCountry]
+        messagebox.showinfo('Answer', wantedCountry + '\'s capital is ' + capital + '!')
+    else:
+        newCapital = simpledialog.askstring('Teach me', 'I don\'t know what is the capital of ' + wantedCountry)
+        world[wantedCountry] = newCapital
+        addData(wantedCountry, newCapital)
+
+root.mainloop()
